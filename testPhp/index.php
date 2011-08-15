4c0a1f3953b929cba5250eb778e28661809df7c7
GALLARDR <roland.gallardo@lhsc.on.ca>
2011-08-15 11:03:05 -0400

    init

---
+++ testPhp/index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Created on Aug 15, 2011
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+ echo "Hello World";
+?>
